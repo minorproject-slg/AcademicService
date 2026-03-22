cb812020b88cda1f94ec1127a52dfb2e4881fdba
minorproject-slg <minorproject.slg@gmail.com>
2026-03-22 23:34:23 +0530

    Initial commit

---
+++ src/main/java/com/minor_project/AcademicService/AcademicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minor_project.AcademicService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minor_project/AcademicService/AcademicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minor_project.AcademicService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
